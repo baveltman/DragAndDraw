b4f635a7c147834e8e312cceca9f844898e3c73b
baveltman <baveltman@gmail.com>
2015-03-28 19:59:07 -0500

    Initial commit

---
+++ app/src/main/java/apps/baveltman/draganddraw/DragAndDrawFragment.java
@@ -0,0 +1,9 @@
+package apps.baveltman.draganddraw;
+
+import android.support.v4.app.ListFragment;
+
+/**
+ * Created by borisvelt on 3/28/15.
+ */
+public class DragAndDrawFragment extends ListFragment {
+}
