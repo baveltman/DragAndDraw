ec6fd8741e0c207d7f24649c17328cf43679fd4a
baveltman <baveltman@gmail.com>
2015-03-28 20:07:29 -0500

    Created class to encapsulate a custom view

--- app/src/main/java/apps/baveltman/draganddraw/DragAndDrawFragment.java
+++ app/src/main/java/apps/baveltman/draganddraw/DragAndDrawFragment.java
@@ -1,9 +1,17 @@
 package apps.baveltman.draganddraw;
 
-import android.support.v4.app.ListFragment;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
-/**
- * Created by borisvelt on 3/28/15.
- */
-public class DragAndDrawFragment extends ListFragment {
+public class DragAndDrawFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup parent,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_drag_and_draw, parent, false);
+        return v;
+    }
 }
